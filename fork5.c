ad2b4121ab780c51be22c5e13600382bd3f07f1f
ArynAgarwal <96115326+ArynAgarwal@users.noreply.github.com>
2022-09-11 18:29:16 +0530

    array child prints even and parent prints odd

---
+++ fork5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+
+	int n;
+	printf("Enter the size of the array: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the elements of the array: ");
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int p = fork();
+	for(int i=0;i<n;i++){
+		if(arr[i]%2==0){
+			if(p==0){
+				printf("You're inside child class and the even elements of the array are: %d\n",arr[i]);
+			}
+		}
+		else{
+			if(p>0){
+				printf("You're inside parent class and the odd elements of the array are: %d\n",arr[i]);
+			}
+		}
+	}
+	return 0;
+}
