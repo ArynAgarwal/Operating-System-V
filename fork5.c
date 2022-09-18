c6d933f183e5ed5146593a8d7d16eb1aaa2a32f7
ArynAgarwal <96115326+ArynAgarwal@users.noreply.github.com>
2022-09-18 23:40:59 +0530

    Odd even commit!

--- fork5.c
+++ fork5.c
@@ -5,7 +5,9 @@ int main(){
 	int n;
 	printf("Enter the size of the array: ");
 	scanf("%d",&n);
+	
 	int arr[n];
+	
 	printf("Enter the elements of the array: ");
 	for(int i=0;i<n;i++){
 		scanf("%d",&arr[i]);
